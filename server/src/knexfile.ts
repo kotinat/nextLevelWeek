3f3ddb9b06e7ce54e6d74beffba769be93daad91
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-02 18:59:22 -0300

    Adicionando o knex e primeira migration

---
+++ server/src/knexfile.ts
@@ -0,0 +1,11 @@
+import path from "path";
+
+// configurações que o bd não tem
+// knex não usa export default!!
+
+module.exports = {
+  client: "sqlite3",
+  connection: {
+    filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+  },
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,55 +1,9 @@
-import express, { response } from "express";
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-// o express não entende json, precisamos passar essa info pra ele
 app.use(express.json());
-
-const users = ["Diego", "Cleiton", "Robson", "Daniel"];
-
-app.get("/users", (request, response) => {
-  // console.log("Listagem de usuários");
-  //response.json(users);
-
-  // usando query param
-  const search = String(request.query.search);
-
-  // é opcional, então faremos um if
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  return response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  console.log("Buscando por id");
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
-
-// app.post("/users", (request, response) => {
-//   const user = {
-//     name: "Diego",
-//     email: "diego@rocketseat.com.br",
-//   };
-
-// return para devolver a resposta e não continuar executando após isso
-//   return response.json(user);
-// });
+app.use(routes);
 
 app.listen(3333);
