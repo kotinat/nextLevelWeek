3a39af608b0d89595c1bdf8005af90bb9919361d
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-02 21:22:16 -0300

    knexfile and migrations

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -8,4 +8,8 @@ module.exports = {
   connection: {
     filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
   },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'), 
+  },
+  useNullAsDefault: true
 };
--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -1,20 +1,13 @@
 import Knex from "knex";
-// tipos do typescript geralmente começam com letra maiúscula
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable("points", (table) => {
+  return knex.schema.createTable("point_items", (table) => {
     table.increments("id").primary();
-    table.string("image").notNullable();
-    table.string("name").notNullable();
-    table.string("email").notNullable();
-    table.string("whatsapp").notNullable();
-    table.decimal("latitude").notNullable();
-    table.decimal("longitude").notNullable();
-    table.string("city").notNullable();
-    table.string("uf", 2).notNullable();
+      
+    table.integer("item_id").notNullable().references("id").inTable("items");
   });
 }
 
 export async function down(knex: Knex) {
-  return knex.schema.dropTable('points');
+  return knex.schema.dropTable("point_items");
 }
