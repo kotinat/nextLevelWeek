cbf56efe4b3e09bcc5c6876befb143e50ce7150e
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-08 08:36:20 -0300

    Listando todos os itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,25 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "hello" });
+// listar todos os itens
+routes.get("/items", async (request, response) => {
+  // query select
+  // depois, checar melhor como fazer query's no knex
+  // precisamos do await, para que ele aguarde a query
+  // para continuar
+  const items = await knex("items").select("*");
+
+  // transformar informações para um novo formato, para o
+  // frnt, por exemplo
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return response.json(serializedItems);
 });
 
 export default routes;
