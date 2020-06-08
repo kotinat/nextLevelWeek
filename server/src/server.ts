fdf2b3f1b087ecf89b67a15c5f8a0db1ec356216
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-08 14:09:14 -0300

    Adicionando Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from 'cors';
 import routes from "./routes";
 import path from 'path';
 
 const app = express();
+
+// utilizado para adicionar um domínio, futuramente
+// permitem que todas as URL's acessem quando 
+// deixamos em branco
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
