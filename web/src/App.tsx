ec4c25bfffed09583c0fbd31f71f8bdd65bd73aa
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-08 15:26:02 -0300

    Limpando exemplos React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0); // retorna [valor estado, retorna func para atualizar valor estado]
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="iha" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Inc</button>
-    </div>
+    <h1>Hello World!</h1>
   );
 }
 
