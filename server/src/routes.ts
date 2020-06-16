0e82625df511f0b5e91bb592cc0828ab711b327e
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-16 09:27:49 -0300

    Configurações do multer

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,18 +1,26 @@
 import express from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 // listar todos os itens
 routes.get("/items", itemsController.index);
 
-// criar ponto de coleta
-routes.post("/points", pointsController.create);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
+
+// criar ponto de coleta
+// routes.post("/points", pointsController.create);
+// a rota que precisa do multer, do upload, é a rota post!
+routes.post("/points", upload.single("image"), pointsController.create);
+// precisamos alterar o JSON pois ele não suporta envio de arquivos
 
 export default routes;
